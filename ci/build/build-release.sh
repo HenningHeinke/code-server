c2473a3af38670ec22859ff24f326b73dad6a11b
Asher <ash@coder.com>
2020-10-29 14:48:01 -0500

    Add more logging around connections

--- ci/build/build-release.sh
+++ ci/build/build-release.sh
@@ -67,7 +67,9 @@ EOF
 bundle_vscode() {
   mkdir -p "$VSCODE_OUT_PATH"
   rsync "$VSCODE_SRC_PATH/yarn.lock" "$VSCODE_OUT_PATH"
-  rsync "$VSCODE_SRC_PATH/out-vscode${MINIFY+-min}/" "$VSCODE_OUT_PATH/out"
+  local min="-min"
+  [[ -z $MINIFY ]] && min=""
+  rsync "$VSCODE_SRC_PATH/out-vscode$min/" "$VSCODE_OUT_PATH/out"
 
   rsync "$VSCODE_SRC_PATH/.build/extensions/" "$VSCODE_OUT_PATH/extensions"
   if [ "$KEEP_MODULES" = 0 ]; then
